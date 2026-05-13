f8dbb34e3bd061792a1c0fe347700df757c75141
Neomi Sule <nsule@umd.edu>
2026-05-13 13:30:55 -0400

    fix: rewrite non-slash path to non-slash destination

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -29,7 +29,7 @@ const nextConfig = {
         },
         {
           source: "/api/:path*",
-          destination: `${backendUrl}/api/:path*/`,
+          destination: `${backendUrl}/api/:path*`,
         },
       ],
     };
